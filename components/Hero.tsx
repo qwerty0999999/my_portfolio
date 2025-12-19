6e10127ff0bd2b4090f8cf10c55f1a5b9dff75df
qwerty0999999 <rijalulfikri437@gmail.com>
2025-12-20 06:01:10 +0700

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;rijalul r, a Next.js Developer based in Croatia.
+            Hi! I&apos;rijalul firki, a Next.js Developer based in Croatia.
           </p>
 
           <a href="#about">
